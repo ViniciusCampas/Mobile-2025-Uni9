92303d7de51c0faa5f6849e9a2b7324fcb1907b1
Vinicius <143036118+ViniciusCampas@users.noreply.github.com>
2025-09-04 00:01:38 +0000

    feat:if statement

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mobile-2025-Uni9
 Projetos feitos em dart/flutter
+ git checkout -b aula-3/comando-if
---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main(){
+    int idade =18;
+    if (idade >= 18)
+    {
+        print('Pode entrar');
+    }else{
+        print ('nao pode ');
+    }
+    print ('fim do programa');
+}
